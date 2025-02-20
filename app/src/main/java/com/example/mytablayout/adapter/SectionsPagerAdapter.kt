f1ac2781b1efdc5390fcd06580b52545671a358b
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2025-02-20 12:18:07 +0800

    add new flow to paging

--- app/src/main/java/com/example/mytablayout/adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/mytablayout/adapter/SectionsPagerAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.mytablayout.adapter
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -9,17 +10,22 @@ import com.example.mytablayout.ProfileFragment
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
 
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
+//        var fragment: Fragment? =
+        val fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
         }
-        return fragment as Fragment
+
+//        when (position) {
+//            0 -> fragment = HomeFragment()
+//            1 -> fragment = ProfileFragment()
+//        }
+        return fragment
     }
 
 }
